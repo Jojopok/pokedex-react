659907cb88ef8de23bb03a9320a1750ede7463c8
Jojopok <zaiajonathan>
2024-03-18 15:22:55 +0100

    add dynamic pokemon img with the array pokemonList

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,28 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+
+    const pokemon = pokemonList[0];
+
     return <div className="bulbazur">
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                 alt="Une image de pokémon"/>
-            <figcaption> Bulbasaur</figcaption>
+            {pokemon.imgSrc ? (<img src={pokemon.imgSrc}
+                                    alt={pokemon.name}/>) : (<p> ???</p>)}
+
+            <figcaption> {pokemon.name}</figcaption>
         </figure>
     </div>
 }
+
+
 
 export default PokemonCard;
