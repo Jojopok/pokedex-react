39bef5499259d48fb7ac527a4a701c6b87e78101
ZAIA Jonathan <jonathan.zaia@scalian.com>
2024-03-19 15:00:57 +0100

    my first props

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,21 @@ import "./components/PokemonCard.css";
 
 
 function App() {
+
+    const pokemonList = [
+        {
+            name: "bulbasaur",
+            imgSrc:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+            name: "mew",
+        },
+    ];
+
     return (
         <div>
-            <PokemonCard/>
+            <PokemonCard pokemon={pokemonList[0]}/>
         </div>
     );
 }
