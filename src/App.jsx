7d2ab65fabe3a7174c66774e32fe0335f6b7efe7
ZAIA Jonathan <jonathan.zaia@scalian.com>
2024-03-19 17:15:43 +0100

    add state for indexing pokemonlist

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,36 @@ import "./components/PokemonCard.css";
 
 function App() {
 
+    const [pokemonIndex, setPokemonIndex] = useState(0);
+
+    const handClickNext = () => {
+        setPokemonIndex(pokemonIndex + 1);
+    }
+
+    const handClickPrev = () => {
+        setPokemonIndex(pokemonIndex - 1);
+    }
+
     const pokemonList = [
         {
             name: "bulbasaur",
             imgSrc:
                 "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+            name: "charmander",
+            imgSrc:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+        },
+        {
+            name: "squirtle",
+            imgSrc:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+        },
+        {
+            name: "pikachu",
+            imgSrc:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
         },
         {
             name: "mew",
@@ -22,8 +47,11 @@ function App() {
 
     return (
         <div>
-            <PokemonCard pokemon={pokemonList[0]}/>
+            <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+            {pokemonIndex > 0 && <button onClick={handClickPrev}>Précédent</button>}
+            {pokemonIndex < pokemonList.length - 1 && <button onClick={handClickNext}>Suivant</button>}
         </div>
+
     );
 }
 export default App;
